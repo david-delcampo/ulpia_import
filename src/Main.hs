8607eb2914e13ecdac89e8c256da2e92b2af945c
David <david@cincoinfo.com>
2017-04-17 14:38:57 +0200

    Main.hs, Importación de dependencias
    - GHC.Generics: Uso de los genéricos en clases propias
    
    - Data.Aeson: Parse del JSON
    - Data.Maybe: Tipo
    
    - Control.Applicative:
    - Control.Monad:
    
    - qualified Data.ByteString.Lazy as B: Tipo usado en la lectura de
    ficheros.
    
    - qualified Data.Text as T: Tipos usados en el modelo de datos
    
    - Network.HTTP.Client: Para la conexión http y envío de datos al backend

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,12 @@
 
 module Main where
  
+import GHC.Generics
+import Data.Aeson
+import Data.Maybe
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Text as T
+
+import Network.HTTP.Client
